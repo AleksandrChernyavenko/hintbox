bcc263d823b43ced908af7ca6a24ba453c02f635
Чернявенко Александр <at@leads.su>
2014-11-24 12:53:34 +0200

    init vendor

---
+++ vendor/mihaildev/yii2-elfinder/AssetsCallBack.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Date: 23.01.14
+ * Time: 0:51
+ */
+
+namespace mihaildev\elfinder;
+
+
+use yii\web\AssetBundle;
+
+class AssetsCallBack extends AssetBundle
+{
+    public $js = [
+        'js/elfinder.callback.js'
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . "/assets";
+        parent::init();
+    }
+} 
