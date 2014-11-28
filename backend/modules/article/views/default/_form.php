d928c4548b7de06ab9941460693345c8c7fa4f5a
Чернявенко Александр <at@leads.su>
2014-11-28 13:10:29 +0200

    CKEditor + ElFinder

--- backend/modules/article/views/default/_form.php
+++ backend/modules/article/views/default/_form.php
@@ -3,6 +3,9 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use mihaildev\ckeditor\CKEditor;
+use mihaildev\elfinder\ElFinder;
+
 /* @var $this yii\web\View */
 /* @var $model backend\models\Article */
 /* @var $form yii\widgets\ActiveForm */
@@ -23,6 +26,20 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'article_decs')->textarea(['rows' => 6]) ?>
 
+    <?= $form->field($model, 'content')->widget(CKEditor::className(), [
+            'editorOptions' => ElFinder::ckeditorOptions('/category/elfinder',[
+                    'roots' => [
+                        [
+                            'baseUrl'=>'@web',
+                            'basePath'=>'@backend/web',
+                            'path' => 'images/uploads',
+                            'name' => 'Glo12321321bal'
+                        ],
+
+                    ]
+                ]),
+        ]);
+    ?>
     <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
 
     <?= $form->field($model, 'origin_url')->textInput(['maxlength' => 255]) ?>
@@ -31,12 +48,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'default_image')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'create')->textInput() ?>
-
-    <?= $form->field($model, 'update')->textInput() ?>
-
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
