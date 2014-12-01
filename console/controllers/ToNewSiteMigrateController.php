61a033e62385b78100ef5c320552f59e601bc665
Чернявенко Александр <at@leads.su>
2014-12-01 10:16:00 +0200

    консольная комманда для перехода на новый сайт

---
+++ console/controllers/ToNewSiteMigrateController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Александр Чернявенко
+ * Date: 01.12.2014
+ * Time: 10:14
+ */
+
+class ToNewSiteMigrateController extends \yii\console\Controller
+{
+
+}
