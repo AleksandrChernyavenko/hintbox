4b8062a5293038df12737a61dc711c70dfc22867
AleksandrChernyavenko <aleksandrchernyavenko@gmail.com>
2014-12-11 22:37:47 +0200

    newerton/yii2-jcrop

--- backend/modules/article/views/default/_form.php
+++ backend/modules/article/views/default/_form.php
@@ -40,6 +40,52 @@ use common\widgets\ElFinder;
 
     <?= $form->field($model, 'default_image')->textarea(['rows' => 6]) ?>
 
+    <?php
+    echo \newerton\jcrop\jCrop::widget([
+        // Image URL
+        'url' =>  \Yii::$app->staticUrlManager->baseUrl . "/images/article/{$model->id}/".$model->default_image,
+        // options for the IMG element
+        'imageOptions' => [
+            'id' => 'imageId',
+            'width' => 600,
+            'alt' => 'Crop this image'
+        ],
+        // Jcrop options (see Jcrop documentation [http://deepliquid.com/content/Jcrop_Manual.html])
+        'jsOptions' => array(
+            'minSize' => [204, 204],
+            'aspectRatio' => 1,
+            'onRelease' => new yii\web\JsExpression("function() {ejcrop_cancelCrop(this);}"),
+            'onChange' => new yii\web\JsExpression("function() { console.log(this) }"),
+            //customization
+            'bgColor' => '#FF0000',
+            'bgOpacity' => 0.4,
+            'selection' => true,
+            'theme' => 'light',
+        ),
+        // if this array is empty, buttons will not be added
+        'buttons' => array(
+            'start' => array(
+                'label' => 'Начать обработку',
+                'htmlOptions' => array(
+                    'class' => 'myClass',
+                    'style' => 'color:red;'
+                )
+            ),
+            'crop' => array(
+                'label' => 'Применить обрезку',
+            ),
+            'cancel' => array(
+                'label' => 'Отменить обрезку'
+            )
+        ),
+        // URL to send request to (unused if no buttons)
+        'ajaxUrl' => 'controller/ajaxcrop',
+        // Additional parameters to send to the AJAX call (unused if no buttons)
+        'ajaxParams' => ['someParam' => 'someValue'],
+    ]);
+    ?>
+
+
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
